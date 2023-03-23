e3a92fb0c29c1ec5cf7d8b75f0c48ab196996d1d
RedRe4per <s3582474@gmail.com>
2023-03-23 19:40:54 +1100

    feat: init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {}, //design system的内容写在这里。文档：https://tailwindcss.com/docs/theme
+  },
+  plugins: [],
+}
