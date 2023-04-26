a393a273fc9b50a1a50bd50dce7a384e8c32fbe8
RedRe4per <s3582474@gmail.com>
2023-04-26 22:33:31 +1000

    feat: robot test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 interface IRobot {
   position: {
-    x: number; 
+    x: number;
     y: number;
   };
   orient: "N" | "S" | "E" | "W";
@@ -11,13 +11,13 @@ interface IRobot {
 }
 
 interface IScent {
-  x: number; 
+  x: number;
   y: number;
 }
 
 interface IPrevPosition {
   position: {
-    x: number; 
+    x: number;
     y: number;
   };
   orient: "N" | "S" | "E" | "W";
@@ -27,8 +27,18 @@ export default function Home() {
   const [xScope, setXScope] = useState(5);
   const [yScope, setYScope] = useState(3);
   const [robots, setRobots] = useState<IRobot[]>([
-    { position: {x: 1, y: 1}, orient:"E", instruction: "RFRFRFRF", isLost: false },
-    { position: {x: 3, y: 2}, orient:"N", instruction: "FRRFLLFFRRFLL", isLost: false }
+    {
+      position: { x: 1, y: 1 },
+      orient: "E",
+      instruction: "RFRFRFRF",
+      isLost: false,
+    },
+    {
+      position: { x: 3, y: 2 },
+      orient: "N",
+      instruction: "FRRFLLFFRRFLL",
+      isLost: false,
+    },
   ]);
   //const [scents, setScents] = useState<IScent[]>([]);
   const scents: any[] = [];
@@ -36,29 +46,29 @@ export default function Home() {
 
   const handleXScope = (e: any) => {
     if (e.target.value > 0 && e.target.value <= 50) {
-      setXScope(e.target.value)
+      setXScope(e.target.value);
     }
-  }
+  };
 
   const handleYScope = (e: any) => {
     if (e.target.value > 0 && e.target.value <= 50) {
-      setYScope(e.target.value)
+      setYScope(e.target.value);
     }
-  }
+  };
 
   const turnRight = (orient: "N" | "S" | "E" | "W") => {
-    if(orient === "N") return "E";
-    if(orient === "E") return "S";
-    if(orient === "S") return "W";
-    if(orient === "W") return "N";
-  }
+    if (orient === "N") return "E";
+    if (orient === "E") return "S";
+    if (orient === "S") return "W";
+    if (orient === "W") return "N";
+  };
 
   const turnLift = (orient: "N" | "S" | "E" | "W") => {
-    if(orient === "N") return "W";
-    if(orient === "W") return "S";
-    if(orient === "S") return "E";
-    if(orient === "E") return "N";
-  }
+    if (orient === "N") return "W";
+    if (orient === "W") return "S";
+    if (orient === "S") return "E";
+    if (orient === "E") return "N";
+  };
 
   // const scentTest = (prevPosition: IPrevPosition) => {
   //   let isScentTested = false;
@@ -85,56 +95,82 @@ export default function Home() {
 
     const position = prevPosition.position;
     let isLost = false;
-    if(prevPosition.orient === "N"){
+    if (prevPosition.orient === "N") {
       position.y = position.y + 1;
-    } else if (prevPosition.orient === "S"){
+    } else if (prevPosition.orient === "S") {
       position.y = position.y - 1;
-    } else if (prevPosition.orient === "E"){
+    } else if (prevPosition.orient === "E") {
       position.x = position.x + 1;
-    } else if (prevPosition.orient === "W"){
+    } else if (prevPosition.orient === "W") {
       position.x = position.x - 1;
     }
 
-    if(position.x < 0 || position.y < 0 || position.x > xScope -1 || position.y > yScope -1){
+    if (
+      position.x < 0 ||
+      position.y < 0 ||
+      position.x > xScope - 1 ||
+      position.y > yScope - 1
+    ) {
       isLost = true;
       scents.push(position);
     }
 
-    console.log("orientation:", prevPosition.orient, "| new position:", position, "| isLost? :", isLost)
+    console.log(
+      "orientation:",
+      prevPosition.orient,
+      "| new position:",
+      position,
+      "| isLost? :",
+      isLost
+    );
 
-    return {position, isLost}
-  }
+    return { position, isLost };
+  };
 
   const handleStart = () => {
-    while(robots.length > 0){
-      console.log("new robot run, instruction:", robots[0].instruction)
-    
-      while(robots[0].instruction.length > 0){
-        console.log("remaining instructions:", robots[0].instruction, robots[0].instruction.length)
-        if(robots[0].instruction.length > 0){
-          if(robots[0].instruction[0] === "R"){
-            robots[0].orient = turnRight(robots[0].orient) as "N" | "S" | "E" | "W";
-          } else if(robots[0].instruction[0] === "L"){
-            robots[0].orient = turnLift(robots[0].orient) as "N" | "S" | "E" | "W";
-          } else if(robots[0].instruction[0] === "F"){
-            const newStatus = getNewPosition({position: robots[0].position, orient: robots[0].orient});
+    while (robots.length > 0) {
+      console.log("new robot run, instruction:", robots[0].instruction);
+
+      while (robots[0].instruction.length > 0) {
+        console.log(
+          "remaining instructions:",
+          robots[0].instruction,
+          robots[0].instruction.length
+        );
+        if (robots[0].instruction.length > 0) {
+          if (robots[0].instruction[0] === "R") {
+            robots[0].orient = turnRight(robots[0].orient) as
+              | "N"
+              | "S"
+              | "E"
+              | "W";
+          } else if (robots[0].instruction[0] === "L") {
+            robots[0].orient = turnLift(robots[0].orient) as
+              | "N"
+              | "S"
+              | "E"
+              | "W";
+          } else if (robots[0].instruction[0] === "F") {
+            const newStatus = getNewPosition({
+              position: robots[0].position,
+              orient: robots[0].orient,
+            });
             robots[0].position = newStatus.position;
             robots[0].isLost = newStatus.isLost;
-            
-            if(newStatus.isLost) break;
+
+            if (newStatus.isLost) break;
           }
-  
+
           robots[0].instruction = robots[0].instruction.slice(1);
         }
-
       }
 
       robotResults.push(robots[0]);
       robots.shift();
     }
 
-    console.log("robotResults:", robotResults, "scents:", scents)
-}
+    console.log("robotResults:", robotResults, "scents:", scents);
+  };
 
   return (
     <>
@@ -142,19 +178,29 @@ export default function Home() {
         <h2>Martian Robots </h2>
         <section className="mt-4">
           <span>X scope: </span>
-          <input className="text-black w-12" type="number" onChange={(e) => handleXScope(e)} value={xScope} />
+          <input
+            className="text-black w-12"
+            type="number"
+            onChange={(e) => handleXScope(e)}
+            value={xScope}
+          />
           <span className="ml-4">Y scope: </span>
-          <input className="text-black w-12" type="number" onChange={(e) => handleYScope(e)} value={yScope} />
+          <input
+            className="text-black w-12"
+            type="number"
+            onChange={(e) => handleYScope(e)}
+            value={yScope}
+          />
         </section>
         <section className="mt-4">
           <p>Robots</p>
         </section>
         <section className="mt-4">
-          <button className="bg-green-500 p-2 rounded-sm" onClick={handleStart}>Start</button>
+          <button className="bg-green-500 p-2 rounded-sm" onClick={handleStart}>
+            Start
+          </button>
         </section>
-        <section className="mt-8">
-          
-        </section>
+        <section className="mt-8"></section>
       </main>
     </>
   );
