27d59e1cef207012271d56f1c5f988ca5911ffe7
RedRe4per <s3582474@gmail.com>
2023-07-17 23:47:16 +1000

    fix: init

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home(item: any = 0) {
   return (
     <>
       <main className="bg-gray-800 text-white h-[100vh] p-12">
-        <h1>Hello Derek! 123</h1>
+        <h1>Hello Derek! 1234</h1>
       </main>
     </>
   );
